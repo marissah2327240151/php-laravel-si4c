6f28f6ce8a2e235eb8a3671de8970eb78d6e07e7
marissah2327240151 <marissahanggitaputri_2327240151@mhs.mdp.ac.id>
2026-05-12 13:22:01 +0700

    INDEX BLADE

--- laravel-si4c/resources/views/fakultas/create.blade.php
+++ laravel-si4c/resources/views/fakultas/create.blade.php
@@ -1,30 +1,49 @@
-<!DOCTYPE html>
+@extends('main')
 
+@section('title', 'Tambah Fakultas')
+
+@section('content')
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6F" crossorigin="anonymous">
+    <title>Fakultas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-    <form action="/fakultas" method="post">
+    <form action="{{ route('fakultas.store')}}" method="post">
         @csrf
         <div class="m-3">
-            <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <h5>Form Pengisian Data Fakultas</h5>
+            <label for="nama" class="form-label">Nama Fakultas</label>
+            <input name="nama" type="text" class="form-control" id="nama" placeholder="Masukkan Nama Fakultas.." value={{old ('nama')}}>
+            @error('nama')
+            <div class="text-danger">{{ $message }}</div>
+                
+            @enderror
+
+            <label for="singkatan" class="form-label">Singkatan Fakultas</label>
+            <input name ="singkatan" type="text" class="form-control" id="singkatan" placeholder="Masukkan Singkatan Fakultas.." value={{old ('singkatan')}}>
+            @error('singkatan')
+            <div class="text-danger">{{ $message }}</div>
+                
+            @enderror
+
+            <label for="dekan" class="form-label">Nama Dekan Fakultas</label>
+            <input name="dekan" type="text" class="form-control" id="dekan" placeholder="Masukkan Nama Dekan.." value={{old ('dekan')}}>
+            @error('dekan')
+            <div class="text-danger">{{ $message }}</div>
+                
+            @enderror
+            <button type="submit" class="btn btn-primary mt-3">Simpan</button>
         </div>
-        <button type="submit">Create</button>
     </form>
+   
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> 
 </body>
+
 </html>
-            <input type="text" name="nama_fakultas" id="nama_fakultas" required>
-        </div>
-        <button type="submit">Create</button>
-    </form>
-</body>
-</html>
+@endsection
--- laravel-si4c/resources/views/fakultas/index.blade.php
+++ laravel-si4c/resources/views/fakultas/index.blade.php
@@ -1,4 +1,7 @@
-<h2>Data Fakultas</h2>
-@foreach ($result as $item)
-    {{$item->nama_fakultas}}- {{$item->singkatan}} - {{$item->dekan}} <br>
-@endforeach
+@extends('main')
+@section('content')
+    <h2>Data Fakultas</h2>
+    @foreach ($result as $item)
+        {{$item->nama_fakultas}}- {{$item->singkatan}} - {{$item->dekan}} <br>
+    @endforeach
+@endsection
