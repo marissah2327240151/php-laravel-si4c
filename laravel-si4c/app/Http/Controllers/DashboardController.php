0a2134937497d916173cb4c68a6754a0df7e4b6b
marissah2327240151 <marissahanggitaputri_2327240151@mhs.mdp.ac.id>
2026-06-02 13:00:06 +0700

    dashboard

--- laravel-si4c/app/Http/Controllers/DashboardController.php
+++ laravel-si4c/app/Http/Controllers/DashboardController.php
@@ -7,17 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        // DB
-        $jumlahMahasiswa = DB::select('SELECT p.nama_prodi, count(*) as jumlah
-        FROM laravelsi4c.mahasiswas m 
-        join laravelsi4c.prodis p
-        on m.prodi_id = p.id
-        GROUP BY p.nama_prodi');
-        return view('dashboard.index', compact('jumlahMahasiswa'));
+        // Grafik jumlah mahasiswa per program studi
+        $grafikmhs = DB::select("
+            SELECT prodis.nama_prodi,
+                   COUNT(*) as jumlah_mhs
+            FROM mahasiswas
+            JOIN prodis
+            ON mahasiswas.prodi_id = prodis.id
+            GROUP BY prodis.nama_prodi
+        ");
+
+        // Grafik jumlah mahasiswa per tahun angkatan
+        $grafikangkatan = DB::select("
+            SELECT LEFT(npm,2) AS tahun_angkatan,
+                   COUNT(*) AS jumlah_mhs
+            FROM mahasiswas
+            GROUP BY LEFT(npm,2)
+            ORDER BY tahun_angkatan
+        ");
+
+        return view('dashboard', compact(
+            'grafikmhs',
+            'grafikangkatan'
+        ));
     }
 }
